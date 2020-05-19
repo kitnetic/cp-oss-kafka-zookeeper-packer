586ef01abaa8458c2364a1650b4b6ee2123f214f
Thomas Remmert <thomas.remmert@kitnetic.co.uk>
2020-05-19 19:14:07 +0100

    initial commit

---
+++ install-confluent-oss.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# Get the key
+wget -qO - https://packages.confluent.io/deb/${CONFLUENT_VERSION}/archive.key | sudo apt-key add -
+
+
+sudo add-apt-repository "deb [arch=amd64] https://packages.confluent.io/deb/${CONFLUENT_VERSION} stable main"
+
+sudo apt-get update
+
+sudo apt-get install confluent-kafka-2.12
+
+mv -f  zookeeper.properties /etc/kafka/zookeeper.properties
